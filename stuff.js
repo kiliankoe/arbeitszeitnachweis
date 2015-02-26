19ce92e23cc6cf7211669842eac1b1589dd58727
Dirk Legler <dirko@dirko.net>
2015-02-26 10:13:40 +0100

    days are now added via javascript

--- stuff.js
+++ stuff.js
@@ -1,3 +1,16 @@
 window.onload = function () {
+	var gesamtzahl = document.getElementById("gesamtzahl");
+	var kalender = gesamtzahl.parentNode;
 
+	for(var i=1; i<=31; i++) {
+		var tag = document.createElement("tr");
+		var th = document.createElement("th");
+		th.innerHTML = i+".";
+		tag.appendChild(th);
+		for(var j=0; j<4; j++) {
+			var td = document.createElement("td");
+			tag.appendChild(td);
+		}
+		kalender.insertBefore(tag, gesamtzahl);
+	}
 }
